3aa2dc499e59d42f3ff102a40b6adeaa97632ca1
Ryfd55 <gubardin1984@mail.ru>
2022-06-08 22:23:08 +0400

    1

--- app/src/main/java/com/example/composition/presentation/ChosseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/ChosseLevelFragment.kt
@@ -11,14 +11,14 @@ import com.example.composition.databinding.FragmentChosseLevelBinding
 class ChosseLevelFragment : Fragment() {
     private var _binding: FragmentChosseLevelBinding? = null
     private val binding: FragmentChosseLevelBinding
-    get() = _binding ?: throw RuntimeException ("FragmentChosseLevelBinding == null")
+        get() = _binding ?: throw RuntimeException("FragmentChosseLevelBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentChosseLevelBinding.inflate(inflater,container,false)
-              return binding.root
+        _binding = FragmentChosseLevelBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -11,15 +11,16 @@ import com.example.composition.databinding.FragmentGameBinding
 class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
-    get() = _binding ?: throw RuntimeException ("FragmentGameBinding == null")
+        get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentGameBinding.inflate(inflater,container,false)
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
